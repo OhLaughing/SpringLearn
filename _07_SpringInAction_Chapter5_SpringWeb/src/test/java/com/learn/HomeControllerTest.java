04d4c6559283fce1999e0d22c5ef6af892d5999d
OhLaughing <bigwanggang@163.com>
2019-03-14 10:58:29 +0800

    Create HomeControllerTest.java

---
+++ _07_SpringInAction_Chapter5_SpringWeb/src/test/java/com/learn/HomeControllerTest.java
@@ -0,0 +1,20 @@
+package com.learn;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class HomeControllerTest {
+
+    @Test
+    public void hello() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc  mockMvc = standaloneSetup(controller).build();
+        mockMvc.perform(get("/hello"))
+                .andExpect(view().name("home"));
+    }
+}
