9c13da73ca3a557125f8aafa9bdd062314650a13
OhLaughing <bigwanggang@163.com>
2019-03-23 09:29:04 +0800

    Update SpitterController.java

--- _13_SpringInAction_Chapter7_MultipartFile/src/main/java/com/learn/SpitterController.java
+++ _13_SpringInAction_Chapter7_MultipartFile/src/main/java/com/learn/SpitterController.java
@@ -5,6 +5,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 import java.io.File;
 import java.io.IOException;
 
+import javax.servlet.http.Part;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -33,37 +36,37 @@ public class SpitterController {
     return "registerForm";
   }
   
-//  @RequestMapping(value="/register", method=POST)
-//  public String processRegistration(
-//      @RequestPart(value="profilePictures", required=false) Part fileBytes,
-//      RedirectAttributes redirectAttributes,
-//      @Valid Spitter spitter,
-//      Errors errors) throws IOException {
-//    if (errors.hasErrors()) {
-//      return "registerForm";
-//    }
-//    
-//    spitterRepository.save(spitter);
-//    redirectAttributes.addAttribute("username", spitter.getUsername());
-//    redirectAttributes.addFlashAttribute(spitter);
-//    return "redirect:/spitter/" + spitter.getUsername();
-//  }
-  
   @RequestMapping(value="/register", method=POST)
   public String processRegistration(
-      @Valid SpitterForm spitterForm,
-      Errors errors) throws IllegalStateException, IOException {
-    
+      @RequestPart(value="profilePictures", required=false) Part fileBytes,
+      RedirectAttributes redirectAttributes,
+      @Valid Spitter spitter,
+      Errors errors) throws IOException {
     if (errors.hasErrors()) {
       return "registerForm";
     }
-    Spitter spitter = spitterForm.toSpitter();
+
     spitterRepository.save(spitter);
-    MultipartFile profilePicture = spitterForm.getProfilePicture();
-    profilePicture.transferTo(new File("/tmp/spittr/" + spitter.getUsername() + ".jpg"));
+    redirectAttributes.addAttribute("username", spitter.getUsername());
+    redirectAttributes.addFlashAttribute(spitter);
     return "redirect:/spitter/" + spitter.getUsername();
   }
   
+//  @RequestMapping(value="/register", method=POST)
+//  public String processRegistration(
+//      @Valid SpitterForm spitterForm,
+//      Errors errors) throws IllegalStateException, IOException {
+//
+//    if (errors.hasErrors()) {
+//      return "registerForm";
+//    }
+//    Spitter spitter = spitterForm.toSpitter();
+//    spitterRepository.save(spitter);
+//    MultipartFile profilePicture = spitterForm.getProfilePicture();
+//    profilePicture.transferTo(new File("/tmp/spittr/" + spitter.getUsername() + ".jpg"));
+//    return "redirect:/spitter/" + spitter.getUsername();
+//  }
+//
   @RequestMapping(value="/{username}", method=GET)
   public String showSpitterProfile(
           @PathVariable String username, Model model) {
