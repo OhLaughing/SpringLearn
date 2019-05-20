922b44641502ae44804d08fa97337966246510aa
OhLaughing <bigwanggang@163.com>
2019-05-20 16:04:46 +0800

    Create Server.java

---
+++ _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/entity/Server.java
@@ -0,0 +1,49 @@
+package com.demo.entity;
+
+public class Server {
+    private int id;
+    private String desc;
+    private String ip;
+    private String port;
+    private String version;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
