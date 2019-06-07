8c3ab10e6b13a2eabfd9635e68327861e55e0974
bigwanggang <bigwanggang@163.com>
2019-06-07 22:40:50 +0800

    add MyBatisLearn files

---
+++ _00_Spring/MyBatisLearn/src/main/java/com/demo/mapper/RoleMapper.java
@@ -0,0 +1,10 @@
+package com.demo.mapper;
+
+import com.demo.pojo.Role;
+
+/**
+ * Created by gustaov on 2019/6/7.
+ */
+public interface RoleMapper {
+    Role selectRole(long id);
+}
---
+++ _00_Spring/MyBatisLearn/src/main/java/com/demo/pojo/Role.java
@@ -0,0 +1,13 @@
+package com.demo.pojo;
+
+import lombok.Data;
+
+/**
+ * Created by gustaov on 2019/6/7.
+ */
+@Data
+public class Role {
+    private Long id;
+    private String roleName;
+    private String note;
+}
