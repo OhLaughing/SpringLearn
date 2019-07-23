f14247c4a363335000f165d10da837acd745fa9f
bigwanggang <bigwanggang@163.com>
2019-07-23 21:03:39 +0800

    Java EE 互联网轻量级框架整合开发

--- _00_Spring/SpringMvc_Mybatis/README.md
+++ _00_Spring/SpringMvc_Mybatis/README.md
@@ -6,5 +6,7 @@
 -   Springmvc 在哪里初始化url和controller方法的对应关系的？
 -   弄清楚HandlerExecutionChain与HanderMapping关系
 -   DispatcherServlet实现了Servlet接口，整个请求处理流：HttpServlet.service -> FrameworkServlet.doGet -> FrameworkServlet.processRequest -> DispatcherServlet.doService -> DispatcherServlet.doDispatch
+-   DispatcherServlet的doDispatch是主要的分发过程
+-   HandlerMapping和HandlerExecutionChain是什么关系,以及与HandlerAdapter什么关系
 ### 好文章
 - [debug方式深入springMVC源码的实现](https://www.jianshu.com/p/fd19e464697d)
--- _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/controller/HelloController.java
+++ _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by gustaov on 2019/5/16.
@@ -22,5 +23,12 @@ public class HelloController {
     @ResponseBody
     public String test() {
         return "this is test";
+    }
+
+    @RequestMapping("index")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
     }
 }
