543140b4bec80146a267c60f0e789fdba4e7ab5c
OhLaughing <bigwanggang@163.com>
2019-08-01 08:19:54 +0800

    Create Result.java

---
+++ _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/entity/Result.java
@@ -0,0 +1,12 @@
+package com.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Result {
+    private boolean result;
+    private String message;
+
+}
