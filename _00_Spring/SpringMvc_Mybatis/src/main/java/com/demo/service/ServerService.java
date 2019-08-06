e90a60e5aa1b44e19e77ac02361f0915f1b7cef2
OhLaughing <bigwanggang@163.com>
2019-08-06 10:22:36 +0800

    Update ServerService.java

--- _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/service/ServerService.java
+++ _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/service/ServerService.java
@@ -1,10 +1,12 @@
 package com.demo.service;
 
+import com.demo.CheckException;
+import com.demo.MmlException;
 import com.demo.entity.Server;
 
 public interface ServerService {
-    int add(Server server);
-    Server delete(int id);
+    int add(Server server)throws MmlException, CheckException;
+    int delete(int id);
     int update(Server server);
     Server find(int id);
 }
