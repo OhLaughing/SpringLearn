5d8df1763070126727146ee22f4be0a8bb7727ba
OhLaughing <bigwanggang@163.com>
2019-08-06 10:24:43 +0800

    Update DataUpdateMml.java

--- _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/telnet/DataUpdateMml.java
+++ _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/telnet/DataUpdateMml.java
@@ -1,22 +1,45 @@
 package com.demo.telnet;
 
 import com.demo.MmlException;
+import com.demo.Utils;
+import com.demo.entity.Task;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
+import java.text.MessageFormat;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 升级命令
+ * DATA UPDATE:PATH="a",SOURCEVERSION="V1",TARGETVERSION="v2"
  * Created by gustaov on 2019/7/31.
  */
+@Slf4j
 public class DataUpdateMml implements MmlExecutor {
-    private static String DATA_UPDATE_MML = "";
-    @Override
-    public String getMml() {
-        return DATA_UPDATE_MML;
+    private static String DATA_UPDATE_MML = "DATA UPDATE:PATH=\"{0}\",SOURCEVERSION=\"{1}\",TARGETVERSION=\"{2}\"";
+    private Task task;
+
+    public DataUpdateMml(Task task) {
+        this.task = task;
     }
 
     @Override
-    public Map<String, Object> parseResult(String result) throws MmlException {
-        return null;
+    public String getMml() {
+        return MessageFormat.format(DATA_UPDATE_MML, task.getFilePath(),
+                task.getSourceVersion(), task.getTargetVersion());
+    }
+
+    @Override
+    public Map<String, String> parseResult(String result) {
+        Map<String, String> map = new HashMap<>();
+        if (!Utils.checkErrorCode(result)) {
+            return map;
+        }
+        result = StringUtils.substringAfter(result, ":");
+        if(result.endsWith(";")){
+            result = StringUtils.substringBefore(result, ";");
+        }
+        return Utils.getMapInfo(result);
     }
 }
