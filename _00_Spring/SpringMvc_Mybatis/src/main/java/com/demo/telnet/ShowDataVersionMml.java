126faa2f71ad7020ac8999862c7c130b95f08e36
OhLaughing <bigwanggang@163.com>
2019-08-06 16:15:01 +0800

    Update ShowDataVersionMml.java

--- _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/telnet/ShowDataVersionMml.java
+++ _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/telnet/ShowDataVersionMml.java
@@ -1,22 +1,33 @@
 package com.demo.telnet;
 
-import com.demo.MmlException;
+import com.demo.Utils;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
  * Created by gustaov on 2019/7/31.
  */
 public class ShowDataVersionMml implements MmlExecutor {
-    private static String SHOW_DATA_VERSION_MML = "";
+    private static String SHOW_DATA_VERSION_MML = "CHECK DATA VERSION";
+
     @Override
     public String getMml() {
         return SHOW_DATA_VERSION_MML;
     }
 
     @Override
-    public Map<String, Object> parseResult(String result) throws MmlException {
-        return null;
+    public Map<String, String> parseResult(String result) {
+
+        Map<String, String> map = new HashMap<>();
+        if (!Utils.checkErrorCode(result)) {
+            return map;
+        }
+        result = StringUtils.substringAfter(result, ":");
+        if(result.endsWith(";")){
+            result = StringUtils.substringBefore(result, ";");
+        }
+        return Utils.getMapInfo(result);
     }
 }
