50f1cbb5cf992c79092601a834f89069bb2e516f
OhLaughing <bigwanggang@163.com>
2019-08-06 16:15:33 +0800

    Update ShowServerVersionMml.java

--- _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/telnet/ShowServerVersionMml.java
+++ _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/telnet/ShowServerVersionMml.java
@@ -1,11 +1,13 @@
 package com.demo.telnet;
 
-import com.demo.MmlException;
+import com.demo.Utils;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ShowServerVersionMml implements MmlExecutor {
-    private static String SHOW_SERVER_VERSION = "SHOW SERVERVERSION";
+    private static String SHOW_SERVER_VERSION = "CHECK SERVER VERSION";
 
     @Override
     public String getMml() {
@@ -13,7 +15,15 @@ public class ShowServerVersionMml implements MmlExecutor {
     }
 
     @Override
-    public Map<String, Object> parseResult(String result) throws MmlException {
-        return null;
+    public Map<String, String> parseResult(String result) {
+        Map<String, String> map = new HashMap<>();
+        if (!Utils.checkErrorCode(result)) {
+            return map;
+        }
+        result = StringUtils.substringAfter(result, ":");
+        if(result.endsWith(";")){
+            result = StringUtils.substringBefore(result, ";");
+        }
+        return Utils.getMapInfo(result);
     }
 }
