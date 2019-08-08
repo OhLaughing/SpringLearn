eabcb36d7fa55e2371e40cfac060513ada159bff
OhLaughing <bigwanggang@163.com>
2019-08-08 19:58:23 +0800

    Update ServerMapper.java

--- _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/mapper/ServerMapper.java
+++ _00_Spring/SpringMvc_Mybatis/src/main/java/com/demo/mapper/ServerMapper.java
@@ -2,9 +2,12 @@ package com.demo.mapper;
 
 import com.demo.entity.Server;
 
+import java.util.List;
+
 public interface ServerMapper {
     int add(Server server);
     int delete(int id);
     int update(Server server);
     Server find(int id);
+    List<Server> findAll();
 }
